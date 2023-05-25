7dfd2db8cfa66b121f2ac8888f0497ff1b1e0e25
Juanse <juansesosa45@gmail.com>
2023-05-25 16:19:03 -0300

    correccion

--- models/Product.js
+++ models/Product.js
@@ -2,11 +2,11 @@ const {model, Schema} = require('mongoose')
 
 const ProductSchema = new Schema({
   id: {type: Number,  unique: true},
-  name: {trype: String},
+  title: {trype: String},
   price: {type: String },
   category: {type: String},
   description: {type: String},
-  img: {type: String},
+  image: {type: String},
 })
 
 module.exports = model('Product', ProductSchema)
